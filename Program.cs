db679f8a4a1c69caa37fe5f76045ccc1aa6e014e
fatcheetah <git@thefatcheetah.com>
2023-11-08 01:51:30 +0000

    Revert "feat(): header of indexes"
    This reverts commit f454872090434c7771bd42c8432df32228e5167a.
    BinaryWriter SeekOrigin.Begin does not shift other builders by index

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ void DownloadPagesZipDeflateContents()
 
         using var archive = new ZipArchive(httpStream, ZipArchiveMode.Read);
         using var memoryStream = new MemoryStream();
-        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, true);
+        using var writer = new BinaryWriter(memoryStream, Encoding.UTF8, false);
 
         var osPath = Environment.OSVersion.Platform switch
         {
@@ -95,8 +95,6 @@ void DownloadPagesZipDeflateContents()
         var osEntries = archive.Entries.Where(e => e.FullName.StartsWith($"{pagePath}{osPath}{Path.DirectorySeparatorChar}"));
         var entries = commonEntries.Concat(osEntries);
 
-        var keys = new StringBuilder();
-
         foreach (var entry in entries)
         {
             var match = entry.FullName.LastIndexOf(Path.DirectorySeparatorChar) + 1;
@@ -105,13 +103,9 @@ void DownloadPagesZipDeflateContents()
             using var streamReader = new StreamReader(entry.Open());
             string contents = streamReader.ReadToEnd();
 
-            keys.Append($"{keyName},");
             writer.Write(keyName);
             writer.Write(contents);
         }
-
-        writer.Seek(0, SeekOrigin.Begin);  // this does prepend and shift the writer
-        writer.Write(keys.ToString());
 
         using var compressStream = File.Open(dataLocation, FileMode.Create);
         using var compresser = new BrotliStream(compressStream, compressionLevel: CompressionLevel.Optimal);
@@ -133,18 +127,7 @@ void GetCommand(string commandName)
     using var decompressor = new BrotliStream(dataFile, CompressionMode.Decompress);
     using var reader = new BinaryReader(decompressor, encoding: Encoding.UTF8, false);
 
-    var commandKeys = reader.ReadString()
-        .Split(",")
-        .OrderBy(c => c)
-        .ToList();
-
-    if (!commandKeys.Contains(commandName))
-    {
-        ConsoleEx.WriteColor($"{commandName} ", ConsoleColor.Yellow);
-        Console.Write("not found \n");
-        return;
-    }
-
+    bool commandFound = false;
     try
     {
         while (true)
@@ -154,6 +137,7 @@ void GetCommand(string commandName)
 
             if (key == commandName)
             {
+                commandFound = true;
                 WriteContentOfFile(value);
                 return;
             }
@@ -162,6 +146,14 @@ void GetCommand(string commandName)
     catch (EndOfStreamException)
     {
         return;
+    }
+    finally
+    {
+        if (!commandFound)
+        {
+            ConsoleEx.WriteColor($"{commandName} ", ConsoleColor.Yellow);
+            Console.Write("not found \n");
+        }
     }
 }
 
@@ -171,19 +163,32 @@ void ListCommands()
     using var decompressor = new BrotliStream(dataFile, CompressionMode.Decompress);
     using var reader = new BinaryReader(decompressor, encoding: Encoding.UTF8, false);
 
-    var commandKeys = reader.ReadString()
-        .Split(",")
-        .OrderBy(c => c)
-        .ToList();
-
-    var sb = new StringBuilder();
-    foreach (var cmd in commandKeys)
+    var list = new SortedSet<string>();
+    try
     {
-        sb.Append($"{cmd}, ");
+        while (true)
+        {
+            var cmd = reader.ReadString();
+            list.Add(cmd);
+
+            reader.ReadString();
+        }
     }
-    ConsoleEx.ColorToggle(ConsoleColor.Yellow);
-    Console.WriteLine($"{sb}\n");
-    ConsoleEx.ColorToggle(ConsoleColor.Yellow);
+    catch (EndOfStreamException)
+    {
+        return;
+    }
+    finally
+    {
+        var sb = new StringBuilder();
+        foreach (var cmd in list)
+        {
+            sb.Append($"{cmd}, ");
+        }
+        ConsoleEx.ColorToggle(ConsoleColor.Yellow);
+        Console.WriteLine($"{sb}\n");
+        ConsoleEx.ColorToggle(ConsoleColor.Yellow);
+    }
 }
 
 void GetRandomCommand()
@@ -192,31 +197,25 @@ void GetRandomCommand()
     using var decompressor = new BrotliStream(dataFile, CompressionMode.Decompress);
     using var reader = new BinaryReader(decompressor, encoding: Encoding.UTF8, false);
 
-    var commandKeys = reader.ReadString()
-        .Split(",")
-        .OrderBy(c => c)
-        .ToList();
-
-    var index = new Random().Next(commandKeys.Count);
-    var commandName = commandKeys[index];
-
+    var list = new List<(string key, string value)>();
     try
     {
         while (true)
         {
             var key = reader.ReadString();
             var value = reader.ReadString();
-
-            if (key == commandName)
-            {
-                WriteContentOfFile(value);
-                return;
-            }
+            list.Add((key, value));
         }
     }
     catch (EndOfStreamException)
     {
         return;
+    }
+    finally
+    {
+        var index = new Random().Next(list.Count);
+        var command = list[index];
+        WriteContentOfFile(command.value);
     }
 }
 
